c7ca0a68c5a0ca0646140eb607f1d153f19c6a0d
karnika2024 <manigayu2024@gmail.com>
2022-05-31 00:10:48 +0530

    C Done

--- src/org/test/SampleJava.java
+++ src/org/test/SampleJava.java
@@ -4,5 +4,8 @@ public class SampleJava {
 public static void main(String[] args) {
 	System.out.println("java");
 	System.out.println(123);
+	System.out.println("Hai");
+	int c=22;
+	System.out.println(c);
 }
 }
